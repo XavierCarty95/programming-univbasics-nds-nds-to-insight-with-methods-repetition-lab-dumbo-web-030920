34dcc30b7a6ce8262f3ed01de60a46076151c848
Xavier Carty <xaviercarty@gmail.com>
2019-12-30 23:19:58 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -45,6 +45,11 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
+   totals = 0 
+   index  = 0 
+   all_dir = directors_totals(source)
+   total_length = list_of_directors(source)
+   while index < 
    
 end
 
