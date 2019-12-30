4a75d62561f0806d2ed60838889dd24f2b322ba6
Xavier Carty <xaviercarty@gmail.com>
2019-12-30 23:24:07 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -49,7 +49,7 @@ def total_gross(source)
    index  = 0
    all_dir = directors_totals(source)
    total_length = list_of_directors(source)
-   while index < all_dir.length do
+   while index < total_length.length do
      totals += all_dir[list_of_directors]
      index +=  1
   end
